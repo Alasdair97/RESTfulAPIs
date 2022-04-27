47c28189f3fb47235a8d5675fba335171d580e6c
Alasdair97 <alasdairw@me.com>
2022-04-27 13:11:50 +0100

    Test of Obj --> JSON

--- VetAPI.py
+++ VetAPI.py
@@ -28,7 +28,7 @@ PetOwners = [
 
 @app.route('/', methods=['GET'])  # tell which HTTP method we are using (GET) and what route (extra bit of the URL) this method will be activated on.  In this case nothing and so home
 def home():
-    return "<h1>Welcome to Gareth's virtual lesson</h1><p>Sorry I am still ill.</p>" #what the api returns
+    return "<h1>Welcome to TSI Vets</h1><p>Internal System</p>" #what the api returns
 
 
 # A route to return all of the available entries in our collection of pet owners.
--- VetInterface.py
+++ VetInterface.py
@@ -1,6 +1,26 @@
 # Vet interface
+import flask
+import json
+from flask import request, jsonify
 from VetAnimal import Dog
 
-Obj = Dog('Tom', 'Steve')
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
+Pet1 = Dog('Tom', 'Steve')
+
+JsonObjectString = json.dumps(Pet1.__dict__)
+
+# print(JsonObjectString) # Test for JSON
+
+
+@app.route('/', methods=['GET'])  # tell which HTTP method we are using (GET) and what route (extra bit of the URL) this method will be activated on.  In this case nothing and so home
+def home():
+    return "<h1>Welcome to TSI Vets</h1><p>Internal System</p>" #what the api returns
+
+
+# A route to return all of the available entries in our collection of pet owners.
+@app.route('/api/somearea/vetcustomers/all', methods=['GET'])
+def api_all():
+    return jsonify(JsonObjectString)
 
