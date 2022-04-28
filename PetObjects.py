ab4fec3fc24696be42f11029621136d620c223d6
Alasdair97 <alasdairw@me.com>
2022-04-28 15:31:39 +0100

    Importing both Owners and Pets from one file

--- PetObjects.py
+++ PetObjects.py
@@ -8,13 +8,16 @@ readfile = txt_file.readlines()
 txt_file.close()
 
 Pets = []
+Owners = []
 
 for i in range(len(readfile)):
     imported_str = readfile[i].strip()
     imported_dict = eval(imported_str)
-    print(imported_dict)
-    if 'Owner_name' in imported_dict:
+    # print(imported_dict)
+    if 'Owner_Name' in imported_dict:
         print('This a human')
+        Owner_i = VetOwner(imported_dict['Owner_Name'])
+        Owners.append(Owner_i)
     elif 'PetName' in imported_dict:
         print('This a pet')
         Pet_i = eval(imported_dict['value'])(imported_dict['PetName'], imported_dict['age'], imported_dict['owner_id'])
@@ -24,16 +27,11 @@ for i in range(len(readfile)):
             Pet_i.colour = imported_dict['colour']
         Pets.append(Pet_i)
     else:
-        print('WTF is this')
-
-Alasdair = VetOwner('Alasdair')
-Ahmed = VetOwner('Ahmed')
-Gareth = VetOwner('Gareth')
+        print('ERROR: Not an Owner or Animal')
 
 # Updates
 Pets[1].vist('being slow even for a dog')
 Pets[3].vist('Dog bite')
 
-PetOwners = [Alasdair, Ahmed, Gareth]
+PetOwners = Owners
 AnimalList = Pets
-print("done")
